9e36289c10c6ecbc67abea2cd4080ed160705e8e
David Wilson <dlw5@st-andrews.ac.uk>
2015-02-03 10:58:52 -0500

    minor fixes

---
+++ Unity/Assets/Character.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+
+    public int Health;
+    public float Damage;
+    public float TurnSpeed;
+    public float Weight;
+    private float moveSpeed = 15;
+    private Vector3 moveDirection;
+
+
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+    }
+}
--- Unity/Assets/Scripts/GravityBody.cs
+++ Unity/Assets/Scripts/GravityBody.cs
@@ -6,16 +6,19 @@ public class GravityBody : MonoBehaviour {
 
 
     GravityAttractor planet;
+    private Transform thisTransform;
 	// Use this for initialization
     void Awake()
     {
         planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<GravityAttractor>();
         rigidbody.useGravity = false;
         rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        thisTransform = transform;
     }
 
     void FixedUpdate()
     {
-        planet.Attract(transform);
+        if (planet) 
+            planet.Attract(thisTransform);
     }
 }
