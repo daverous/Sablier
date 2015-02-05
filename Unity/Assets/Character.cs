6b13e89e3288fe1245fa481acf68957f46bdc3ad
David Wilson <dlw5@st-andrews.ac.uk>
2015-02-05 15:35:00 -0500

    made change to char file

--- Unity/Assets/Character.cs
+++ Unity/Assets/Character.cs
@@ -8,15 +8,33 @@ public class Character : MonoBehaviour {
     public float Damage;
     public float TurnSpeed;
     public float Weight;
+    public float jumpForce = 500f;
     private float moveSpeed = 15;
     private Vector3 moveDirection;
-
+    private bool isGrounded = false;
 
     void Update()
     {
         moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            rigidbody.AddForce(0, jumpForce,0);
     }
 
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.name == "planet")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision info)
+    {
+        if (info.collider.name == "planet")
+        {
+            isGrounded = false;
+        }
+    }
     void FixedUpdate()
     {
         rigidbody.MovePosition(rigidbody.position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
