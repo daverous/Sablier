2866e073b798ef34da5128cd8438941dc25aef7b
David Wilson <dlw5@st-andrews.ac.uk>
2015-03-04 16:52:53 -0500

    correctly implemented hits

---
+++ Unity/Assets/hits1Script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class hits1Script : MonoBehaviour {
+    Text hits;
+    Character character;
+	// Use this for initialization
+	void Start () {
+	    if (tag =="Text1") {
+            character = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>();
+        }
+        
+        if (tag == "Text2")
+        {
+            character = GameObject.FindGameObjectWithTag("Player2").GetComponent<Character>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float userHits = character.getHits();
+            hits.text = ("hits:" + userHits.ToString());
+            hits.text = ("hits:" + userHits.ToString());
+    }
+}
