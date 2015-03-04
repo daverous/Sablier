76c9667578d1d6af6e4d8ea93ad059a410343791
BBTMaster <Wilson@MacBook-Pro.local>
2015-03-04 14:59:16 -0500

    adding image

---
+++ Unity/Assets/CanvasScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasScript : MonoBehaviour {
+	Canvas canvas;
+	public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		canvas = GetComponent<Canvas>();
+		canvas.worldCamera = camera; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
