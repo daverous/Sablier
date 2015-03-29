0430862f6ee9097de81ef352aa611a56d787fe91
David Wilson <dlw5@st-andrews.ac.uk>
2015-03-29 13:13:11 -0400

    added power ups

--- Unity/Assets/AsteroidScript.cs
+++ Unity/Assets/AsteroidScript.cs
@@ -1,28 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AsteroidScript : MonoBehaviour {
-    public float asteroidDamage = 15f;
-	// Use this for initialization
-	void Start () {
+public class AsteroidScript : MonoBehaviour
+{
+		public float asteroidDamage = 15f;
+		// Use this for initialization
+		void Start ()
+		{
 	
-	}
-    void OnCollisionEnter(Collision other)
-    {
-        string tag = other.transform.root.tag;
-        // If asteroid has hit player
-        if (tag == "Player" || tag == "Player2")
-        {
-            Character player = GameObject.FindGameObjectWithTag(tag).GetComponent<Character>();
-            player.beenHit(asteroidDamage);
-        }
-        if (tag == "Planet")
-        {
-            gameObject.GetComponent<Detonator>().Explode(); 
-        }
-    }
-	// Update is called once per frame
-	void Update () {
+		}
+		void OnCollisionEnter (Collision other)
+		{
+				string tag = other.transform.root.tag;
+				// If asteroid has hit player
+				if (tag == "Player" || tag == "Player2") {
+						Character player = GameObject.FindGameObjectWithTag (tag).GetComponent<Character> ();
+						player.beenHit (asteroidDamage);
+				}
+				if (tag == "Planet" || tag == "PowerUp") {
+						gameObject.GetComponent<Detonator> ().Explode (); 
+				}
+		}
+		// Update is called once per frame
+		void Update ()
+		{
 	
-	}
+		}
 }
--- Unity/Assets/PowerUpScript.cs
+++ Unity/Assets/PowerUpScript.cs
@@ -1,21 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PowerUpScript : MonoBehaviour {
+public class PowerUpScript : MonoBehaviour
+{
 
-	public float powerUpAmount = 0;
-
-	// Use this for initialization
-	void Start () {
+		public float powerUpAmount = 0;
+		public float rotateSpeed = 10f;
+		// Use this for initialization
+		void Start ()
+		{
 	
-	}
+		}
 
-	public float getPowerUpAmount() {
-		return powerUpAmount;
+		public float getPowerUpAmount ()
+		{
+				return powerUpAmount;
 		}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+		// Update is called once per frame
+		void Update ()
+		{
+				transform.Rotate (Vector3.up, rotateSpeed * Time.deltaTime);
+		}
 }
