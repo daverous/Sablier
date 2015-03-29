6fde4132702fb2ebeec68ca17068744529e7c3f3
RedShep <constantkingship@gmail.com>
2015-03-29 15:41:05 -0400

    Go Scene and IntroScene

---
+++ Unity/Assets/Done/Done_Scripts/SlideOutOnClickA.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlideOutOnClickA : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) { 
+			animator.SetBool("aIsPressed", true);
+				}
+	//	anim.SetTrigger("StartOnClick");
+		if (animator.GetCurrentAnimatorStateInfo(0).IsName("pressaslideoutstay")){
+			Application.LoadLevel ("TestScene");
+			}
+	}
+}
