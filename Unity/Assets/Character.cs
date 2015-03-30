06c4dd34bed09fb4ffc67a2cd8e70521f4b3249e
David Wilson <dlw5@st-andrews.ac.uk>
2015-03-29 21:19:28 -0400

    gameovertest

--- Unity/Assets/Character.cs
+++ Unity/Assets/Character.cs
@@ -316,32 +316,43 @@ public class Character : MonoBehaviour
 						GameManager gm = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();
 						//increment wins of other player
 
-						int currentRound = StaticStore.getNumberOfRounds();
+                        //int currentround = staticstore.getnumberofrounds();
 						
-//						//Check if its only one round that has been played and return the winner.
-//						if (currentRound == 1) {
-//							if (gameObject.tag == "Player2") {
-//								gm.IncrementPlayerOneWins ();
-//								Application.LoadLevel ("RoundWonPlayer1");
-//							}
-//							if (gameObject.tag == "Player") {
-//								gm.IncrementPlayerTwoWins ();
-//								Application.LoadLevel ("RoundWonPlayer2");
-//							}
-//						} 
+                        ////check if its only one round that has been played and return the winner.
+                        //if (currentround == 1) {
+                        //    if (gameobject.tag == "player2") {
+                        //        gm.incrementplayeronewins ();
+                        //        application.loadlevel ("roundwonplayer1");
+                        //    }
+                        //    if (gameobject.tag == "player") {
+                        //        gm.incrementplayertwowins ();
+                        //        application.loadlevel ("roundwonplayer2");
+                        //    }
+                        //} 
 
-					//	if (currentRound == 3){
-							if (gameObject.tag == "Player2") {
-									gm.IncrementPlayerOneWins ();
-									Application.LoadLevel ("GameOverFinalPlayer1");
-							}
-							if (gameObject.tag == "Player") {
-									gm.IncrementPlayerTwoWins ();
-									Application.LoadLevel ("GameOverFinalPlayer2");
-							}
-					//	}
+                        //if (currentround == 3){
+                        //    if (gameobject.tag == "player2") {
+                        //            gm.incrementplayeronewins ();
+                        //            application.loadlevel ("gameoverfinalplayer1");
+                        //    }
+                        //    if (gameobject.tag == "player") {
+                        //            gm.incrementplayertwowins ();
+                        //            application.loadlevel ("gameoverfinalplayer2");
+                        //    }
+                        //}
 
-						//Application.LoadLevel ("GOScene");
+                        if (gameObject.tag == "Player2")
+                        {
+                            gm.IncrementPlayerOneWins();
+                            Application.LoadLevel("GameOverFinalPlayer1");
+                        }
+                        if (gameObject.tag == "Player")
+                        {
+                            gm.IncrementPlayerTwoWins();
+                            Application.LoadLevel("GameOverFinalPlayer2");
+                        }
+
+						//Application.LoadLevel ("GameOverFinalPlayer1");
 				}
 				animator.SetBool ("Damaged", true);
 				
