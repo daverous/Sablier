b8348f0d24507d63b459944a14c8e41676cb9bc7
4AllThatNeverWas <fp12pk@student.ocadu.ca>
2015-04-02 01:19:01 -0400

    FlameThrower. BITCH.

--- Unity/Assets/PipeBlade.cs
+++ Unity/Assets/PipeBlade.cs
@@ -4,20 +4,36 @@ using System.Collections;
 public class PipeBlade : MonoBehaviour {
 	private Animator animator;
 	private Animator anim;
+	private GameObject[] fires;
 	// Use this for initialization
 	void Start () {
 		animator = this.GetComponent<Animator> ();
 		anim = this.transform.root.GetComponent<Animator>();
+		fires = GameObject.FindGameObjectsWithTag("FirePart");
+		foreach (GameObject fire in fires) {
+			fire.SetActive(false);
+		}
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+
 		if(anim.GetCurrentAnimatorStateInfo(0).IsName("PipeBlade|QuickForward") ||
 		   anim.GetCurrentAnimatorStateInfo(0).IsName("PipeBlade|QuickBack") ||
 		   anim.GetCurrentAnimatorStateInfo(0).IsName("PipeBlade|Heavy")){
+			foreach (GameObject fire in fires) {
+				fire.SetActive(true);
+			}
 			animator.SetBool("Torch", true);
+
 		}else{
 			animator.SetBool("Torch", false);
+			foreach (GameObject fire in fires) {
+				fire.SetActive(false);
+			}
 		}
 	}
 	void OnCollisionEnter (Collision other){
